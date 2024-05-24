bdaadd7b89df5ee47830ecd0a86031ff205bf8b9
RyanMichael Hirst <ryanmichaelhirst@gmail.com>
2024-05-23 22:30:46 -0400

    Add seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,18 @@
 import { db } from "@/lib/db.server"
+import { fakerEN_US as faker } from "@faker-js/faker"
 
-async function seed() {}
+async function seed() {
+  const iterations = Array.from(Array(10).keys())
+  for (const iter of iterations) {
+    await db.user.create({
+      data: {
+        email: faker.internet.email(),
+        name: faker.person.fullName(),
+        googleId: faker.string.numeric(10),
+      },
+    })
+  }
+}
 
 seed()
   .then(async () => {
